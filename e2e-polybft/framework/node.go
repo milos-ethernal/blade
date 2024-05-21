f6ffe0f65de120fab3fbf94cbdea10f931bf83c6
Igor Crevar <crewce@gmail.com>
2024-05-21 14:44:19 +0200

    zoltans cardano shutdown fix

--- e2e-polybft/framework/node.go
+++ e2e-polybft/framework/node.go
@@ -5,13 +5,20 @@ import (
 	"os"
 	"os/exec"
 	"sync/atomic"
+	"time"
+)
+
+const (
+	numberOfInterruptsOnForceStop       = 5
+	shutdownGracePeriodOnForceStopInSec = 10
 )
 
 type Node struct {
-	shuttingDown atomic.Bool
-	cmd          *exec.Cmd
-	doneCh       chan struct{}
-	exitResult   *exitResult
+	shuttingDown    atomic.Bool
+	cmd             *exec.Cmd
+	doneCh          chan struct{}
+	exitResult      *exitResult
+	shouldForceStop bool
 }
 
 func NewNode(binary string, args []string, stdout io.Writer) (*Node, error) {
@@ -30,6 +37,10 @@ func NewNode(binary string, args []string, stdout io.Writer) (*Node, error) {
 	go n.run()
 
 	return n, nil
+}
+
+func (n *Node) SetShouldForceStop(shouldForceStop bool) {
+	n.shouldForceStop = shouldForceStop
 }
 
 func (n *Node) ExitResult() *exitResult {
@@ -66,7 +77,21 @@ func (n *Node) Stop() error {
 	}
 
 	n.shuttingDown.Store(true)
-	<-n.Wait()
+
+	if n.shouldForceStop {
+		// give it time to shutdown gracefully
+		select {
+		case <-n.Wait():
+		case <-time.After(time.Second * shutdownGracePeriodOnForceStopInSec):
+			for i := 0; i < numberOfInterruptsOnForceStop; i++ {
+				_ = n.cmd.Process.Signal(os.Interrupt)
+			}
+
+			<-n.Wait()
+		}
+	} else {
+		<-n.Wait()
+	}
 
 	return nil
 }
